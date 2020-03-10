ba46e4f1a57e44e1b297a665cfd70719a6305b09
tbngoc0910 <yttranngoc.hv@gmail.com>
2020-03-10 09:52:49 +0700

    expression example

---
+++ Lab3/expression.php
@@ -0,0 +1,12 @@
+<html>
+    <head> <title>Expression Example </title> </head>
+    <body>
+     <?php
+     $first_num = 12;
+     $second_num = 356;
+     $third_num = 11 ;
+     $total = $first_num + $second_num + $third_num;
+     print ("total= $total");
+     ?>
+    </body> 
+</html>
